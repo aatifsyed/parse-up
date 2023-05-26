2e62f79445d4df494648bf6af240bbcb1d4a15e1
Aatif Syed <aatifsyedyp@gmail.com>
2023-05-26 02:33:09 +0100

    feat: initial implementation
    UpParser UpResult, tag, misc utils

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,109 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fmt::Display;
+
+use itertools::Itertools;
+
+use util::{chars_needed_to_complete, go_on, oops, yes_and};
+
+pub mod util;
+
+macro_rules! oops {
+    ($tt:tt) => {
+        Err(UpError::Oops {
+            message: format!($tt),
+        })
+    };
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub type UpResult<'input, T> = Result<YesAnd<'input, T>, UpError>;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+/// Successful parse so far.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub struct YesAnd<'input, T> {
+    /// The value.
+    pub yes: T,
+    /// The remaining input.
+    pub and: &'input str,
+    /// Other valid options.
+    pub could_also: Vec<String>,
+}
+
+/// Parsing couldn't continue.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum UpError {
+    /// Parse error.
+    Oops { message: String },
+    /// Suggestions to append to the current input which would make parsing succeed.
+    /// You should only return this when you're at the end of the input.
+    GoOn { go_on: Vec<String> },
+}
+
+pub trait UpParser<'input, T> {
+    fn parse(&self, input: &'input str) -> UpResult<'input, T>;
+}
+
+impl<'input, T, F> UpParser<'input, T> for F
+where
+    F: Fn(&'input str) -> UpResult<'input, T>,
+{
+    fn parse(&self, input: &'input str) -> UpResult<'input, T> {
+        self(input)
     }
+}
+
+/// Takes the string `tag` from the input.
+/// ```
+/// use parse_up::{UpParser as _, tag, util::{yes_and, go_on, oops}};
+/// assert_eq!(
+///     tag("hello").parse(""),
+///     go_on(["hello"]),
+/// );
+/// assert_eq!(
+///     tag("hello").parse("hell"),
+///     go_on(["o"]),
+/// );
+/// assert_eq!(
+///     tag("hello").parse("hello"),
+///     yes_and("hello", ""),
+/// );
+/// assert_eq!(
+///     tag("hello").parse("hello, world!"),
+///     yes_and("hello", ", world!"),
+/// );
+/// assert_eq!(
+///     tag("hello").parse("world"),
+///     oops("expected hello, not world"),
+/// );
+/// ```
+pub fn tag<'input, 'tag>(tag: &'tag str) -> impl UpParser<'input, &'input str> + 'tag {
+    move |input: &'input str| match input.strip_prefix(tag) {
+        Some(rest) => yes_and(&input[..tag.len()], rest),
+        None => match chars_needed_to_complete(tag, input) {
+            Some("") => unreachable!("would've been caught in prefix"),
+            Some(suggestion) => go_on([suggestion]),
+            None => oops!("expected {tag}, not {input}"),
+        },
+    }
+}
+
+pub fn dictionary<'input, KeyT, ValueT>(
+    items: impl IntoIterator<Item = (KeyT, ValueT)>,
+) -> impl UpParser<'input, ValueT>
+where
+    KeyT: Display,
+    ValueT: Clone,
+{
+    let pairs = items
+        .into_iter()
+        .map(|(k, v)| (k.to_string(), v))
+        // largest keys first
+        .sorted_by_key(|(k, _v)| std::cmp::Reverse(k.clone()))
+        .collect::<Vec<_>>();
+    move |input: &str| todo!()
+}
+
+pub fn and_then<'input, L, R>(
+    left: impl UpParser<'input, L>,
+    right: impl UpParser<'input, R>,
+) -> impl UpParser<'input, (L, R)> {
+    move |input| todo!()
 }
---
+++ src/util.rs
@@ -0,0 +1,108 @@
+use std::fmt::Display;
+
+use itertools::Itertools as _;
+
+use crate::{UpError, UpResult, YesAnd};
+
+pub fn yes_and<T>(yes: T, and: &str) -> UpResult<T> {
+    Ok(YesAnd {
+        yes,
+        and,
+        could_also: vec![],
+    })
+}
+
+pub fn yes_and_also<T, AlsoT: Display>(
+    yes: T,
+    and: &str,
+    also: impl IntoIterator<Item = AlsoT>,
+) -> UpResult<T> {
+    Ok(YesAnd {
+        yes,
+        and,
+        could_also: also.into_iter().map(|a| a.to_string()).collect(),
+    })
+}
+
+pub fn go_on<'any, AnyT, GoOnT: Display>(
+    suggestions: impl IntoIterator<Item = GoOnT>,
+) -> UpResult<'any, AnyT> {
+    Err(UpError::GoOn {
+        go_on: suggestions.into_iter().map(|g| g.to_string()).collect(),
+    })
+}
+
+pub fn oops<'any, AnyT, MessageT: Display>(message: MessageT) -> UpResult<'any, AnyT> {
+    Err(UpError::Oops {
+        message: message.to_string(),
+    })
+}
+
+macro_rules! oops {
+    ($tt:tt) => {
+        Err(UpError::Oops {
+            message: format!($tt),
+        })
+    };
+}
+
+/// ```
+/// use parse_up::util::strip_matching_prefixes;
+/// assert_eq!(
+///     strip_matching_prefixes("hel", ["hello", "helsinki"]),
+///     ["lo", "sinki"],
+/// );
+/// assert_eq!(
+///     strip_matching_prefixes("bar", ["bartender", "bar"]),
+///     ["tender"],
+/// );
+/// ```
+pub fn strip_matching_prefixes<'haystack>(
+    needle: &str,
+    haystack: impl IntoIterator<Item = &'haystack str>,
+) -> Vec<String> {
+    haystack
+        .into_iter()
+        .filter_map(|hay| {
+            hay.strip_prefix(needle).and_then(|s| match s {
+                "" => None,
+                _ => Some(s),
+            })
+        })
+        .map(String::from)
+        .collect()
+}
+
+/// What characters would need to be added to `haystack` to get to `needle`?
+/// returns [None] if wouldn't be possible.
+/// ```
+/// use parse_up::util::chars_needed_to_complete;
+/// assert_eq!(chars_needed_to_complete("tag", "ta" ), Some("g"));
+/// assert_eq!(chars_needed_to_complete("tag", ""   ), Some("tag"));
+/// assert_eq!(chars_needed_to_complete("tag", "tag"), Some("")); // Special case
+/// assert_eq!(chars_needed_to_complete("tag", "tar"), None);
+/// assert_eq!(chars_needed_to_complete(""   , "foo"), None);
+/// ```
+pub fn chars_needed_to_complete<'needle>(
+    needle: &'needle str,
+    haystack: &str,
+) -> Option<&'needle str> {
+    use itertools::EitherOrBoth::{Both, Left, Right};
+    for chars in needle.char_indices().zip_longest(haystack.chars()) {
+        match chars {
+            Both((_, l), r) if l == r => continue, // matched
+            Both(..) => return None,               // diverged
+            Left((ix, _)) => return Some(&needle[ix..]),
+            Right(_) => return None,
+        }
+    }
+    Some("") // all chars have matched
+}
+
+#[test]
+fn test_chars_needed_to_complete() {
+    assert_eq!(chars_needed_to_complete("tag", "ta"), Some("g"));
+    assert_eq!(chars_needed_to_complete("foo", ""), Some("foo"));
+    assert_eq!(chars_needed_to_complete("foo", "bar"), None);
+    assert_eq!(chars_needed_to_complete("foo", "food"), None);
+}
