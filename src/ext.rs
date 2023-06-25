aefc4b6a5293758a79e57d5a96e3dd5c9581210d
Aatif Syed <aatifsyedyp@gmail.com>
2023-06-25 16:00:09 +0100

    feat: specialized MapYes, add borrowed

--- src/ext.rs
+++ src/ext.rs
@@ -1,25 +1,64 @@
-use crate::{UpParser, YesAnd};
+use std::marker::PhantomData;
+
+use crate::{util::assert_up_parser, UpParser, UpResult, YesAnd};
 
 pub trait UpParserExt<'input, Out>: UpParser<'input, Out> {
-    // TODO(aatifsyed): avoid the allocation by specializing or using impl trait in trait
-    fn map_yes<'a, MapFn, Out2>(mut self, mut map_fn: MapFn) -> Box<dyn UpParser<'input, Out2> + 'a>
+    // RUST(aatifsyed): use impl trait in trait
+    fn map_yes<MapFn, MappedOut>(self, map_fn: MapFn) -> MapYes<Self, Out, MapFn>
     where
-        Self: Sized + 'a,
-        MapFn: FnMut(Out) -> Out2 + 'a,
+        MapFn: FnMut(Out) -> MappedOut,
+        Self: Sized,
     {
-        Box::new(move |input| match self.parse_up(input) {
+        assert_up_parser(MapYes {
+            parser: self,
+            parser_out: PhantomData,
+            map_fn,
+        })
+    }
+    // BUG?(aatifsyed): is this necessary?
+    fn borrowed(&mut self) -> Borrowed<'_, Self> {
+        // assert_up_parser::<'input, Out, _>(Borrowed(self))
+        Borrowed(self)
+    }
+}
+
+impl<'input, Out, Parser> UpParserExt<'input, Out> for Parser where Parser: UpParser<'input, Out> {}
+
+pub struct MapYes<Parser, ParserOut, MapFn> {
+    parser: Parser,
+    parser_out: PhantomData<ParserOut>,
+    map_fn: MapFn,
+}
+
+impl<'input, ParserOut, MappedOut, Parser, MapFn> UpParser<'input, MappedOut>
+    for MapYes<Parser, ParserOut, MapFn>
+where
+    Parser: UpParser<'input, ParserOut>,
+    MapFn: FnMut(ParserOut) -> MappedOut,
+{
+    fn parse_up(&mut self, input: &'input str) -> UpResult<'input, MappedOut> {
+        match self.parser.parse_up(input) {
             Ok(YesAnd {
                 yes,
                 and,
                 suggestions,
             }) => Ok(YesAnd {
-                yes: map_fn(yes),
+                yes: (self.map_fn)(yes),
                 and,
                 suggestions,
             }),
             Err(e) => Err(e),
-        })
+        }
     }
 }
 
-impl<'input, Out, Parser> UpParserExt<'input, Out> for Parser where Parser: UpParser<'input, Out> {}
+pub struct Borrowed<'parser, Parser: ?Sized>(&'parser mut Parser);
+
+impl<'input, Out, Parser> UpParser<'input, Out> for Borrowed<'_, Parser>
+where
+    Parser: UpParser<'input, Out>,
+{
+    fn parse_up(&mut self, input: &'input str) -> UpResult<'input, Out> {
+        self.0.parse_up(input)
+    }
+}
